d3595a07bdc89693333bd46edca189064d35858c
provemsu <nikita-burg@mail.ru>
2026-05-26 22:34:18 +0300

    feat: progress bar

--- src/cli/TtyLine.cpp
+++ src/cli/TtyLine.cpp
@@ -1,7 +1,12 @@
+#include "Style.hpp"
 #include "TtyLine.hpp"
+#include <Log.hpp>
 #include <chrono>
+#include <iomanip>
 #include <iostream>
 #include <locale>
+#include <sstream>
+#include <string>
 #include <sys/ioctl.h>
 #include <unistd.h>
 
@@ -31,17 +36,26 @@ constexpr std::string_view kClearLine = "\r\033[K";
 
 using clock = std::chrono::steady_clock;
 constexpr auto kMinInterval = std::chrono::milliseconds(50);
-clock::time_point g_last_render = clock::time_point{};
+clock::time_point g_last_render_line = clock::time_point{};
+clock::time_point g_last_render_bar = clock::time_point{};
+
+bool updatePerm(clock::time_point &last_render) {
+  // return true;
+  if (!g_is_tty)
+    return false;
+  auto now = clock::now();
+  if (now - last_render >= kMinInterval) {
+    last_render = now;
+    return true;
+  }
+  return false;
+}
 
 } // namespace
 
 void updateLine(std::string_view label, std::string_view body) {
-  if (!g_is_tty)
+  if (!updatePerm(g_last_render_line))
     return;
-  auto now = clock::now();
-  if (now - g_last_render < kMinInterval)
-    return;
-  g_last_render = now;
 
   size_t width = getTtyWidth();
   size_t budget = width > label.size() ? width - label.size() : 20;
@@ -68,6 +82,51 @@ Cursor::Cursor() {
 Cursor::~Cursor() {
   if (g_is_tty)
     show_cursor();
+}
+
+ProgressBar::ProgressBar(size_t total) : total_(total) {}
+
+void ProgressBar::updateBar(std::string_view label, size_t done) {
+  if (!updatePerm(g_last_render_bar))
+    return;
+  done_ = done;
+  float procent = calcProcent();
+  std::string bar = getBar(procent);
+
+  std::string spinner = style::colored(
+      std::string(1, spinner_[spinner_state % 4]), style::yellow());
+  spinner_state++;
+  std::ostringstream oss;
+  oss << std::fixed << std::setprecision(2) << procent << "% (" << std::setw(6)
+      << done_ << "/" << std::setw(6) << total_ << ")";
+  std::string stat = oss.str();
+
+  std::string full_line = spinner + " " + label.data() + bar + " " + stat;
+  std::cout << kClearLine << full_line << std::flush;
+}
+
+void ProgressBar::finishProgress(std::string_view final_msg) {
+  finish(final_msg);
+}
+
+float ProgressBar::calcProcent() {
+  return static_cast<float>(done_) / total_ * 100.0f;
+}
+
+std::string ProgressBar::getBar(float procent) {
+  size_t progress = static_cast<size_t>(procent / 100.0f * 20);
+  std::string bar = "[";
+  for (size_t i = 0; i < 20; ++i) {
+    if (i < progress) {
+      bar += '#';
+    } else {
+      bar += '-';
+    }
+  }
+  bar += ']';
+
+  bar[21] = ']';
+  return bar;
 }
 
 } // namespace tty_line
--- src/cli/TtyLine.hpp
+++ src/cli/TtyLine.hpp
@@ -1,6 +1,8 @@
 #ifndef CLI_TTYLINE_HPP_
 #define CLI_TTYLINE_HPP_
 
+#include <array>
+#include <string>
 #include <string_view>
 
 namespace tty_line {
@@ -17,6 +19,22 @@ public:
   Cursor(const Cursor &) = delete;
   Cursor &operator=(const Cursor &) = delete;
 };
+
+class ProgressBar {
+public:
+  explicit ProgressBar(size_t total);
+  void updateBar(std::string_view label, size_t done);
+  void finishProgress(std::string_view final_msg);
+
+private:
+  float calcProcent();
+  std::string getBar(float procent);
+  const std::array<char, 4> spinner_ = {'|', '/', '-', '\\'};
+  const size_t total_;
+  size_t done_;
+  size_t spinner_state = 0;
+};
+
 } // namespace tty_line
 
 #endif // CLI_TTYLINE_HPP_
